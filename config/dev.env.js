531bbfb29241acac90ca3ad7b50caf399007a812
KrBaIO3 <krbaio3@gmail.com>
2018-03-15 14:09:13 +0100

    clean

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// If you don't need more config, add and merge with other environments
-const merge = require('webpack-merge');
-const envProd = require('./prod.env');
-
-module.exports = merge(envProd, {
-  NODE_ENV: '"development"'
-});
