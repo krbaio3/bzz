afed02815d1a3c42dbcb6bf3f2efb0e7206b6858
KrBaIO3 <krbaio3@gmail.com>
2018-03-19 15:31:20 +0100

    chore(migration)
    Migracion a module Heroes (building)

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+// Servicios
+import { HeroesService } from '../service/heroes.service';
+// Componentes
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchComponent } from './components/search/search.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { CapitalizePipe } from '../pipes/capitalizado.pipe';
+import { DomseguroPipe } from '../pipes/dom-seguro.pipe';
+import { PasswordPipe } from '../pipes/password.pipe';
+
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [CommonModule,
+    HeroesRoutingModule
   ],
-  declarations: []
+  declarations: [
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    SearchComponent,
+    PipesComponent,
+    CapitalizePipe,
+    DomseguroPipe,
+    PasswordPipe
+  ]
 })
-export class HeroesModule { }
+export class HeroesModule {}
