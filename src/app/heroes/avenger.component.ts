53c9dbaf87513e8cc88e598beddd242eb5ae6406
KrBaIO3 <krbaio3@gmail.com>
2018-03-27 22:20:59 +0200

    5: Auto stash before merge of "ng5" and "origin/ng5"

---
+++ src/app/heroes/avenger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avenger',
+  templateUrl: './avenger.component.html',
+  styleUrls: ['./avenger.component.scss']
+})
+export class AvengerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
