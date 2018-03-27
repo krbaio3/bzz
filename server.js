e5dbfe13e38ae1996211fe86b8553460bf8254cb
KrBaIO3 <krbaio3@gmail.com>
2018-03-27 17:18:32 +0200

    chore(server.js)

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const connect = require('connect');
 const serveStatic = require('serve-static');
 
-const dirWebRoot = ('/dist');
+const dirWebRoot = __dirname + '/dist';
 const port = Number(process.env.PORT || 5000);
+// const server_host = '0.0.0.0';
 
 console.log(`Este es el dirWebRoot ${dirWebRoot}`);
 
@@ -11,6 +12,7 @@ const app = connect();
 // Decimos a serverStatic que use el directorio dirWebRoot
 // Se lo agregamos a la instancia de connect
 app.use(serveStatic(dirWebRoot));
+
 // Le decimos que escuche por el puerto de la variable port
 app.listen(port);
 
@@ -25,7 +27,13 @@ console.log(`static web server running on port: ${port}`);
 // Decirle que use la carpeta de estaticos dist
 // app.use(express.static(__dirname + '/dist'));
 
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
 // Arranque de app. por el puerto que diga Heroku o el 5000
-// app.listen(process.env.PORT || 5000);
+// app.listen(port, server_host, () => {
+//   console.log(`Listening on port ${port}`);
+// });
 
 // con http-server tambien se puede configurar....
