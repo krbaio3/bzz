435aecdc4a212defdf3e92c4f134fca973e66eca
KrBaIO3 <krbaio3@gmail.com>
2018-03-27 17:32:53 +0200

    chore(Heroku)
    server.js

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-const connect = require('connect');
-const serveStatic = require('serve-static');
+// const connect = require('connect');
+// const serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
 
-const dirWebRoot = __dirname + '/dist';
+const host = '0.0.0.0';
+const baseWebRoot = __dirname;
 const port = Number(process.env.PORT || 5000);
-// const server_host = '0.0.0.0';
+
+const app = express();
 
 console.log(`Este es el dirWebRoot ${dirWebRoot}`);
 
-const app = connect();
-// Por partes más claro:
-// Decimos a serverStatic que use el directorio dirWebRoot
-// Se lo agregamos a la instancia de connect
-app.use(serveStatic(dirWebRoot));
+// const app = connect();
+// // Por partes más claro:
+// // Decimos a serverStatic que use el directorio dirWebRoot
+// // Se lo agregamos a la instancia de connect
+// app.use(serveStatic(dirWebRoot));
 
-// Le decimos que escuche por el puerto de la variable port
-app.listen(port);
+// // Le decimos que escuche por el puerto de la variable port
+// app.listen(port);
 
-console.log(`static web server running on port: ${port}`);
+// console.log(`static web server running on port: ${port}`);
+
 
 // Otra opción:
 
 // Instalar servidor expressjs
-// const express = require('express');
-// const app = express();
 
 // Decirle que use la carpeta de estaticos dist
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(baseWebRoot +'/dist'));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(baseWebRoot + '/dist/index.html'));
+});
 
 // Arranque de app. por el puerto que diga Heroku o el 5000
-// app.listen(port, server_host, () => {
-//   console.log(`Listening on port ${port}`);
-// });
+app.listen(port, host, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 // con http-server tambien se puede configurar....
