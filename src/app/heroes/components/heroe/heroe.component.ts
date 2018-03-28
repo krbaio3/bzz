8e18945f5901e2f960c588954466379fbb4ec2aa
KrBaIO3 <krbaio3@gmail.com>
2018-03-28 11:34:32 +0200

    feat(routes)
    fix routes Heroes

--- src/app/heroes/components/heroe/heroe.component.ts
+++ src/app/heroes/components/heroe/heroe.component.ts
@@ -5,13 +5,16 @@ import { HeroesService } from '../../service/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.scss'],
+  styleUrls: ['./heroe.component.scss']
 })
 export class HeroeComponent implements OnInit {
   heroe: any = {};
 
-  constructor(private activatedRoute: ActivatedRoute, private _heroeService: HeroesService) {
-    activatedRoute.params.subscribe((params) => {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _heroeService: HeroesService
+  ) {
+    activatedRoute.params.subscribe(params => {
       console.log(params['id']);
       this.heroe = this._heroeService.getHeroe(params['id']);
       console.log(this.heroe);
@@ -19,4 +22,5 @@ export class HeroeComponent implements OnInit {
   }
 
   ngOnInit() {}
+
 }
