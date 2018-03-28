00182adb332984e0b342226b9803b82b13c6b622
KrBaIO3 <krbaio3@gmail.com>
2018-03-28 12:37:17 +0200

    feat(coment)
    coment log

--- server.js
+++ server.js
@@ -14,20 +14,20 @@ console.log(`Este es el baseWebRoot ${baseWebRoot}`);
 
 // Vamos a requerir del modulo que provee Node.js 
 // llamado child_process
-let exec = require('child_process').exec, child;
+// let exec = require('child_process').exec, child;
 // Creamos la función y pasamos el string pwd 
 // que será nuestro comando a ejecutar
-child = exec('ls',
+// child = exec('ls',
 // Pasamos los parámetros error, stdout la salida 
 // que mostrara el comando
-  function (error, stdout, stderr) {
+  // function (error, stdout, stderr) {
     // Imprimimos en pantalla con console.log
-    console.log(stdout);
+    // console.log(stdout);
     // controlamos el error
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
+    // if (error !== null) {
+      // console.log('exec error: ' + error);
+    // }
+// });
 
 
 // const app = connect();
