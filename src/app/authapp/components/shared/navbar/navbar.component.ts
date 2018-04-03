b04bd796a4be5dd99bd66560bcb2e1458e70f606
KrBaIO3 <krbaio3@gmail.com>
2018-04-03 20:58:30 +0200

    merge

---
+++ src/app/authapp/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth-service.service';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor( public auth: AuthService) {
+    auth.handleAuthentication();
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.auth.login();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+}
