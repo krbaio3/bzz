3991d0d42cff1f7dc1495f7a5f71890aeefd5061
KrBaIO3 <krbaio3@gmail.com>
2018-04-04 12:50:29 +0200

    feat(clean double)

--- src/app/formularios/formulario.component.ts
+++ src/app/formularios/formulario.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
 
--- src/app/heroes/avenger.component.ts
+++ src/app/heroes/avenger.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-avenger',
   templateUrl: './avenger.component.html',
-  styleUrls: ['./avenger.component.scss']
 })
 export class AvengerComponent implements OnInit {
 
