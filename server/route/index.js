c7a964bc39205313d103189aa8fe1fbcabcb8c77
KrBaIO3 <krbaio3@gmail.com>
2018-04-04 11:36:25 +0200

    feat(server)
    Nebular mock

--- server/route/index.js
+++ server/route/index.js
@@ -1,20 +1,27 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const api = express.Router();
 const routes = {
-    heroes: {
-        url: '/heroes',
-        file: '../api/heroes.mock.json',
-    },
+  heroes: {
+    url: '/auth/sign-in',
+    file: '../api/heroes.mock.json'
+  },
+  nebular: {
+    url: '/auth/sign-in',
+    file: '../api/nebular.mock.json'
+  }
 };
 
 /**
  * Router de Cliente
  */
- api.get(routes.heroes.url, (req, res) => {
-    res.send(JSON.stringify(require(routes.heroes.file)));
+api.get(routes.heroes.url, (req, res) => {
+  res.send(JSON.stringify(require(routes.heroes.file)));
 });
 
+api.post(routes.nebular.url, (req, res) => {
+  res.send(JSON.stringify(require(routes.nebular.file)));
+});
 
 module.exports = api;
